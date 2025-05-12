d6133148797c03693acedf529a2c09b5b9758a3e
Sebastian Mayorga <smayorga5@udi.edu.co>
2025-05-12 18:04:34 -0500

    regiones

--- mostrar_especie.js
+++ mostrar_especie.js
@@ -5,17 +5,18 @@ const router = express.Router();
 // GET /mostrar_especie
 router.get('/mostrar_especie', (req, res) => {
   const sql = `
-    SELECT 
-      e.nombre_comun AS nombre_comun,
-      e.nombre_cientifico AS nombre_cientifico,
-      e.familia AS familia,
-      e.uso AS uso,
-      r.nombre AS region
-    FROM especie e 
-    JOIN especie_subparcela es ON e.id_especie = es.id_especie
-    JOIN subparcela s ON s.id_subparcela = es.id_subparcela
-    JOIN conglomerado c ON c.id_conglomerado = s.id_conglomerado
-    JOIN region r ON r.id_region = c.id_region
+  SELECT 
+    e.nombre_comun AS nombre_comun,
+    e.nombre_cientifico AS nombre_cientifico,
+    e.familia AS familia,
+    e.uso AS uso,
+    LISTAGG(DISTINCT r.nombre, ', ') WITHIN GROUP (ORDER BY r.nombre) AS region
+  FROM especie e 
+  JOIN especie_subparcela es ON e.id_especie = es.id_especie
+  JOIN subparcela s ON s.id_subparcela = es.id_subparcela
+  JOIN conglomerado c ON c.id_conglomerado = s.id_conglomerado
+  JOIN region r ON r.id_region = c.id_region
+  GROUP BY e.nombre_comun, e.nombre_cientifico, e.familia, e.uso;
   `;
 
   connection.query(sql, (err, results) => {
