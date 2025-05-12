6b0875fcdef992522c030d4e05ab6d5bb23c5ee5
Sebastian Mayorga <smayorga5@udi.edu.co>
2025-05-12 16:28:48 -0500

    region 2

--- mostrar_especie.js
+++ mostrar_especie.js
@@ -10,11 +10,12 @@ router.get('/mostrar_especie', (req, res) => {
       e.nombre_cientifico AS nombre_cientifico,
       e.familia AS familia,
       e.uso AS uso,
-      c.nombre AS region
+      r.nombre AS region
     FROM especie e 
     JOIN especie_subparcela es ON e.id_especie = es.id_especie
     JOIN subparcela s ON s.id_subparcela = es.id_subparcela
     JOIN conglomerado c ON c.id_conglomerado = s.id_conglomerado
+    JOIN region r ON r.id_region = c.id_region
   `;
 
   connection.query(sql, (err, results) => {
