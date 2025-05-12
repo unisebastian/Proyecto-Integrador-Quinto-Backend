eb4694b338c4ed06bb2a9d26350616bf3f63cc8b
Sebastian Mayorga <smayorga5@udi.edu.co>
2025-05-12 16:07:56 -0500

    nombre

--- mostrar_imagen.js
+++ mostrar_imagen.js
@@ -4,10 +4,10 @@ const router = express.Router();
 
 // GET /imagen_arbol/:id
 router.get('/imagen_especie/:nombre_comun', (req, res) => {
-  const id = req.params.id;
+  const nombre_comun = req.params.nombre_comun;
   const sql = 'SELECT imagen_arbol FROM imagen_especie WHERE nombre = ?';
 
-  connection.query(sql, [id], (err, results) => {
+  connection.query(sql, [nombre_comun], (err, results) => {
     if (err || results.length === 0) {
       return res.status(404).send('Imagen no encontrada');
     }
