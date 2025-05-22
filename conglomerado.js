f75c81c43b7c9610b79bd6fdc14159f5fefadc15
Sebastian Mayorga <smayorga5@udi.edu.co>
2025-05-22 09:53:56 -0500

    .

--- conglomerado.js
+++ conglomerado.js
@@ -5,26 +5,26 @@ const pool = require('./conexion.js');
 router.get('/mostrar_conglomerado', async (req, res) => {
   try {
     const query = `
-      SELECT 
-        c.identificador,
-        TO_CHAR(c.fecha_creacion, 'YYYY-MM-DD') AS fecha_creacion,
-        r.nombre AS region,
-        m.nombre AS municipio,
-        string_to_array(c.coordenadas, ',')::float8[] AS coordenadas,
-        STRING_AGG(DISTINCT e.nombre_comun, ', ' ORDER BY e.nombre_comun) AS especies
-      FROM conglomerado c
-      JOIN region r ON r.id_region = c.id_region
-      JOIN municipio m ON m.id_municipio = c.id_municipio
-      JOIN subparcela s ON s.id_conglomerado = c.id_conglomerado
-      JOIN arbol a ON a.id_subparcela = s.id_subparcela
-      JOIN especie e ON a.id_especie = e.id_especie
-      GROUP BY 
-        c.identificador,
-        c.fecha_creacion,
-        r.nombre,
-        m.nombre,
-        c.coordenadas
-    `;
+  SELECT 
+    c.identificador,
+    TO_CHAR(c.fecha_creacion, 'YYYY-MM-DD') AS fecha_creacion,
+    r.nombre AS region,
+    m.nombre AS municipio,
+    string_to_array(c.coordenadas, ',')::float8[] AS coordenadas,
+    STRING_AGG(DISTINCT e.nombre_comun, ', ' ORDER BY e.nombre_comun) AS especies
+  FROM conglomerado c
+  JOIN region r ON r.id_region = c.id_region
+  JOIN municipio m ON m.id_municipio = c.id_municipio
+  LEFT JOIN subparcela s ON s.id_conglomerado = c.id_conglomerado
+  LEFT JOIN arbol a ON a.id_subparcela = s.id_subparcela
+  LEFT JOIN especie e ON a.id_especie = e.id_especie
+  GROUP BY 
+    c.identificador,
+    c.fecha_creacion,
+    r.nombre,
+    m.nombre,
+    c.coordenadas
+`;
 
     const { rows } = await pool.query(query);
     res.json(rows);
